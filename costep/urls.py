3933952fa7af55bad33c7be5bd0878a400bbe366
Ben Stueck <wbstueck@gmail.com>
2015-04-24 10:57:12 -0700

    simple url file

--- costep/urls.py
+++ costep/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'costep.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('info.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
